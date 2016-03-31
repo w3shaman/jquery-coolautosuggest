93f3efb2767d506d5cfb79d00d7940507fdcef27
w3shaman <bogeyman2007@gmail.com>
2016-03-31 20:34:26 +0700

    Update the example to use mysqli_* instead of mysql_*.

--- data.php
+++ data.php
@@ -4,23 +4,29 @@ header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 header("Content-type: application/json");
 
 $host="localhost";
-$username="root";
-$password="123xyz";
-$database="lab";
+$username="test";
+$password="123456";
+$database="test";
 
-$con=mysql_connect($host,$username,$password) or die(mysql_error());
-mysql_select_db($database,$con) or die(mysql_error());
+$con=mysqli_connect($host,$username,$password,$database);
 
 $arr=array();
-$result=mysql_query("SELECT * FROM people WHERE name LIKE '%".mysql_real_escape_string($_GET['chars'])."%' ORDER BY name LIMIT 0, 10",$con) or die(mysql_error());
-if(mysql_num_rows($result)>0){
-    while($data=mysql_fetch_row($result)){
-        // Store data in array
-        $arr[]=array("id" => $data[0], "data" => $data[1], "thumbnail" => 'thumbnails/'.$data[3], "description" => $data[2]);
+$result=mysqli_query($con,"SELECT * FROM people WHERE name LIKE '%".mysqli_real_escape_string($con,$_GET['chars'])."%' ORDER BY name LIMIT 0, 10");
+if(mysqli_num_rows($result)>0){
+    while($data=mysqli_fetch_row($result)){
+        // Store data in array 
+        // You can add any additional fields to be used by the autosuggest callback function 
+        $arr[]=array(
+            "id" => $data[0],
+            "data" => $data[1],
+            "thumbnail" => 'thumbnails/'.$data[3],
+            "description" => $data[2],
+            // Additional fields (if any)...
+        );
     }
 }
 
-mysql_close($con);
+mysqli_close($con);
 
 // Encode it with JSON format
 echo json_encode($arr);
