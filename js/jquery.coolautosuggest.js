cf3ca8608e596ad4f91bb5bd8b32f6f993889e57
w3shaman <bogeyman2007@gmail.com>
2016-04-27 21:30:02 +0700

    Optimizing the code. Change some properties access to private. They are unnecesarily declared as public.

--- js/jquery.coolautosuggest.js
+++ js/jquery.coolautosuggest.js
@@ -8,18 +8,18 @@
  */
 
 (function($) {
-  function autosuggest(settings, textField){
-    this.divId="suggestions_holder";
-    this.hovered=false;
-    this.arrData=null;
+  function autosuggest(settings, txtField){
+    var divId="suggestions_holder";
+    var hovered=false;
+    var arrData=null;
 
-    this.textField=textField;
+    var textField=txtField;
 
-    this.textField.after('<div class="suggestions" id="' + this.divId + '"></div>');
-    this.textField.attr("autocomplete", "off");
+    textField.after('<div class="suggestions" id="' + divId + '"></div>');
+    textField.attr("autocomplete", "off");
 
-    this.holder=this.textField.next("#" + this.divId);
-    this.holder.hide();
+    var holder=textField.next("#" + divId);
+    holder.hide();
 
     // Default width is textfield width + 3px
     if (settings.width == null) {
@@ -28,7 +28,7 @@
 
     // Prevent ENTER default action if needed.
     if(settings.idField!=null || settings.submitOnSelect==true || settings.preventEnter==true){
-      this.textField.keypress(
+      textField.keypress(
         function(e){
           if(e.keyCode==13){
             return false;
@@ -45,7 +45,7 @@
     var additionalFields = "";
 
     var me=this;
-    this.textField.keyup(
+    textField.keyup(
       function(e){
         if(e.keyCode!=37 && e.keyCode!=38 && e.keyCode!=39 && e.keyCode!=40 && e.keyCode!=13){
           if($(this).val().length>=settings.minChars){
@@ -61,11 +61,11 @@
               success:function(data){
                 try{
                   if (typeof data == 'string')
-                    me.arrData=$.parseJSON(data);
+                    arrData=$.parseJSON(data);
                   else if (typeof data == 'object')
-                    me.arrData=data;
+                    arrData=data;
 
-                  var arr=me.arrData;
+                  var arr=arrData;
                   var html="";
 
                   currRow=0;
@@ -106,44 +106,44 @@
                           }
                         }
 
-                        html+='<div id="' + suggestRow + (i+1) + '" class="' + cssClass + '"' + id_field + ' seq_id="' + i + '" >' + thumb + '<div class="suggestion_title">' + arr[i].data.replace(new RegExp('(' + me.textField.val() + ')', 'gi'), "<b>$1</b>") + '</div>' + desc + '</div>';
+                        html+='<div id="' + suggestRow + (i+1) + '" class="' + cssClass + '"' + id_field + ' seq_id="' + i + '" >' + thumb + '<div class="suggestion_title">' + arr[i].data.replace(new RegExp('(' + textField.val() + ')', 'gi'), "<b>$1</b>") + '</div>' + desc + '</div>';
                       }
 
-                      me.holder.html(html);
+                      holder.html(html);
 
                       for(i=1;i<=arr.length;i++){
-                        var target=me.holder.find("#" + suggestRow + i);
+                        var target=holder.find("#" + suggestRow + i);
                         target.mouseover(function(e){
-                          me.hovered=true;
+                          hovered=true;
                           me.unSelectAll(this);
                           $(this).addClass("selected");
                         });
 
                         target.mouseout(function(e){
-                          me.hovered=false;
+                          hovered=false;
                           $(this).removeClass("selected");
                         });
 
                         target.click(function(e){
-                          me.textField.val($(this).find(".suggestion_title").text());
+                          textField.val($(this).find(".suggestion_title").text());
                           if(settings.idField!=null){
                             settings.idField.val($(this).attr("id_field"));
                           }
 
                           // Callback function
                           if(typeof settings.onSelected == "function"){
-                            settings.onSelected.call(this, me.arrData[$(this).attr("seq_id")]);
+                            settings.onSelected.call(this, arrData[$(this).attr("seq_id")]);
                           }
 
                           if(settings.submitOnSelect==true){
-                            $("form").has(me.textField).submit();
+                            $("form").has(textField).submit();
                           }
 
                           me.hide();
                         });
                       }
 
-                      me.show(me.holder.find("." + suggestItem).height() * arr.length);
+                      me.show(holder.find("." + suggestItem).height() * arr.length);
                     }
                     else{
                       me.hide();
@@ -168,7 +168,7 @@
           }
         }
         else{
-          if(me.holder.css("display")!="none"){
+          if(holder.css("display")!="none"){
             checkKey(e);
           }
           else{
@@ -181,55 +181,55 @@
       }
     );
 
-    this.textField.bind(
+    textField.bind(
       "blur",
       function(e){
         if(settings.idField!=null){
-          if(me.checkSelected(me.textField.val())==false){
-            me.textField.val("");
+          if(me.checkSelected(textField.val())==false){
+            textField.val("");
             settings.idField.val("");
           }
         }
 
-        if(me.hovered==false){
+        if(hovered==false){
           me.hide();
         }
         else{
-          me.hovered=false;
+          hovered=false;
         }
       }
     );
 
     this.show=function(height){
-      this.holder.css({
+      holder.css({
         "position":"absolute",
-        "left":this.textField.position().left + "px",
-        "top":this.textField.position().top + this.textField.height() + 5 + "px",
+        "left":textField.position().left + "px",
+        "top":textField.position().top + textField.height() + 5 + "px",
         "height":height + "px"
       });
 
-      this.holder.css({
+      holder.css({
         "width":settings.width + "px"
       });
 
-      this.holder.find("." + suggestItem).css({
+      holder.find("." + suggestItem).css({
         "width":settings.width + "px",
         "overflow":"hidden"
       });
 
-      this.holder.show();
+      holder.show();
     }
 
     this.hide=function(){
-      this.holder.hide();
+      holder.hide();
     }
 
     this.unSelectAll=function(div){
       var id=$(div).attr("id");
-      var rows=this.holder.find("." + suggestItem).get().length;
+      var rows=holder.find("." + suggestItem).get().length;
 
       for(i=1;i<=rows;i++){
-        this.holder.find("#" + suggestRow + i).removeClass("selected");
+        holder.find("#" + suggestRow + i).removeClass("selected");
       }
 
       currRow=parseInt(id.replace(suggestRow, ""));
@@ -240,9 +240,9 @@
     }
 
     this.checkSelected=function(data){
-      if(this.arrData!=null){
-        for(var i=0;i<this.arrData.length;i++){
-          if(this.arrData[i].data==data){
+      if(arrData!=null){
+        for(var i=0;i<arrData.length;i++){
+          if(arrData[i].data==data){
             return true;
           }
         }
@@ -252,19 +252,19 @@
     }
 
     function checkKey(e){
-      if(me.holder.css("display")!="none"){
-        var rows=me.holder.find("." + suggestItem).get().length;
+      if(holder.css("display")!="none"){
+        var rows=holder.find("." + suggestItem).get().length;
         if(e.keyCode==40){
           currRow++;
           if(currRow<=rows){
             if(currRow>0){
-              me.holder.find("#" + suggestRow + (currRow-1)).removeClass("selected");
+              holder.find("#" + suggestRow + (currRow-1)).removeClass("selected");
             }
 
-            var target=me.holder.find("#" + suggestRow + currRow);
+            var target=holder.find("#" + suggestRow + currRow);
 
             target.addClass("selected");
-            me.textField.val(target.find(".suggestion_title").text());
+            textField.val(target.find(".suggestion_title").text());
             if(settings.idField!=null){
               settings.idField.val(target.attr("id_field"));
             }
@@ -277,13 +277,13 @@
           currRow--;
           if(currRow>0){
             if(currRow<rows){
-              me.holder.find("#" + suggestRow + (currRow+1)).removeClass("selected");
+              holder.find("#" + suggestRow + (currRow+1)).removeClass("selected");
             }
 
-            var target=me.holder.find("#" + suggestRow + currRow);
+            var target=holder.find("#" + suggestRow + currRow);
 
             target.addClass("selected");
-            me.textField.val(target.find(".suggestion_title").text());
+            textField.val(target.find(".suggestion_title").text());
             if(settings.idField!=null){
               settings.idField.val(target.attr("id_field"));
             }
@@ -294,8 +294,8 @@
         }
         else if(e.keyCode==13){
           if(settings.idField!=null){
-            if(me.checkSelected(me.textField.val())==false){
-              me.textField.val("");
+            if(me.checkSelected(textField.val())==false){
+              textField.val("");
               settings.idField.val("");
             }
           }
@@ -303,7 +303,7 @@
           // Callback function
           if(typeof settings.onSelected == "function"){
             if(currRow>0){
-              settings.onSelected.call(this, me.arrData[currRow-1]);
+              settings.onSelected.call(this, arrData[currRow-1]);
             }
             else{
               settings.onSelected.call(this, null);
